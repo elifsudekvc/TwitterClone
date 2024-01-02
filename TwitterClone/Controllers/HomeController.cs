fc6c64648722290737bb2d269146ef5730eb75fa
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-01-02 17:43:44 +0300

    tweetPostDelete

--- TwitterClone/Controllers/HomeController.cs
+++ TwitterClone/Controllers/HomeController.cs
@@ -27,8 +27,39 @@ namespace TwitterClone.Controllers
                 return Json(tweet, JsonRequestBehavior.AllowGet);
             }
         }
+        public async Task<JsonResult> GetTweet(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"{_apiBaseUrl}/{id}");
+                response.EnsureSuccessStatusCode();
+                var tweet = await response.Content.ReadAsAsync<Tweet>();
+                return Json(tweet, JsonRequestBehavior.AllowGet);
+            }
+        }
 
+        [HttpPost]
+        public async Task<JsonResult> AddTweet(Tweet newTweet)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsJsonAsync(_apiBaseUrl, newTweet);
+                response.EnsureSuccessStatusCode();
+                var addedTweet = await response.Content.ReadAsAsync<Tweet>();
+                return Json(addedTweet);
+            }
+        }
 
-
+        [HttpDelete]
+        public async Task<JsonResult> DeleteTweet(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.DeleteAsync($"{_apiBaseUrl}/{id}");
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsAsync<string>();
+                return Json(new { Message = result });
+            }
+        }
     }
 }
--- TwitterClone/Controllers/TweetApiController.cs
+++ TwitterClone/Controllers/TweetApiController.cs
@@ -49,6 +49,95 @@ namespace TwitterClone.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("api/tweet/{id}")]
+        public IHttpActionResult GetTweetById(int id)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(_connectionString))
+            {
+                dbConnection.Open();
+
+                var query = @"
+            SELECT 
+            t.TweetId, t.TweetContent, t.TweetImg, t.TweetTime,
+            u.UserId AS UserId, u.UserName, u.UserLink, u.UserDescription, u.UserImg
+            FROM Tweet t
+            JOIN [User] u ON t.UserId = u.UserId
+            WHERE t.TweetId = @TweetId
+        ";
+
+                var tweetWithUser = dbConnection.Query<Tweet, User, Tweet>(
+                    query,
+                    (tweet, user) =>
+                    {
+                        tweet.User = user;
+                        return tweet;
+                    },
+                    new { TweetId = id },
+                    splitOn: "UserId"
+                ).FirstOrDefault();
+
+                if (tweetWithUser != null)
+                {
+                    return Ok(tweetWithUser);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult AddTweet(Tweet newTweet)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(_connectionString))
+            {
+                dbConnection.Open();
+
+                var query = @"
+                    INSERT INTO Tweet (TweetContent, TweetImg, TweetTime, UserId)
+                    VALUES (@TweetContent, @TweetImg, GETDATE(),(SELECT [UserId] FROM [dbo].[User] WHERE [UserName] = 'Sude'));
+                    SELECT CAST(SCOPE_IDENTITY() as int)
+                ";
+
+                var tweetId = dbConnection.Query<int>(query, newTweet).Single();
+                newTweet.TweetId = tweetId;
+
+                return Ok(newTweet);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/tweet/{id}")]
+        public IHttpActionResult DeleteTweet(int id)
+        {
+            try
+            {
+                using (SqlConnection dbConnection = new SqlConnection(_connectionString))
+                {
+                    dbConnection.Open();
+
+                    var affectedRows = dbConnection.Execute("DELETE FROM Tweet WHERE TweetId = @TweetId", new { TweetId = id });
+
+                    if (affectedRows > 0)
+                    {
+                        return Ok("Tweet deleted successfully");
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to delete Tweet");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
 
     }
 }
